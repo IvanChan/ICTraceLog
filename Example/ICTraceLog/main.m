2182da0fc34c8875079361c13093956e14418193
_ivanC <aintivanc@icloud.com>
2017-05-12 15:14:21 +0800

    Initial commit

---
+++ Example/ICTraceLog/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ICTraceLog
+//
+//  Created by aintivanc@icloud.com on 05/12/2017.
+//  Copyright (c) 2017 aintivanc@icloud.com. All rights reserved.
+//
+
+@import UIKit;
+#import "ICAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ICAppDelegate class]));
+    }
+}
